00c57f2a76e1c412044309d52b3ed4f95a89e533
g3dox <dimplingprego@proton.me>
2023-08-03 11:31:14 +0200

    made a small refactoring

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,47 @@ use std::{thread, time};
 use std::process;
 
 
+const ASCII_COFFEE: [&str; 22] = [
+    r"                        (",
+    r"                          )     (",
+    r"                   ___...(-------)-....___",
+    r#"               .-""       )    (          ""-."#,
+    r"         .-'``'|-._             )         _.-|",
+    r#"        /  .--.|   `""---...........---""`   |"#,
+    r"       /  /    |                             |",
+    r"       |  |    |                             |",
+    r"        \  \   |                             |",
+    r"         `\ `\ |                             |",
+    r"           `\ `|                             |",
+    r"           _/ /\                             /",
+    r"          (__/  \                           /",
+    r#"       _..---""` \                         /`""---.._"#,
+    r"    .-'           \                       /          \'-.",
+    r"   :               `-.__             __.-'              :",
+    r#"   :                  ) ""---...---"" (                 :"#,
+    r#"    '._               `"--...___...--"`              _.'"#,
+    r#"      \""--..__                              __..--""/"#,
+    r#"       '._     """----.....______.....----"""     _.'"#,
+    r#"          `""--..,,_____            _____,,..--""`"#,
+    r#"                        `"""----"""`"#,
+];
+
+const ASCII_COFFEE_MACHINE: [&str; 12] = [
+    r"  __________________________________________",
+    r" (___________   __________________________  |",
+    r"   [XXXXX]   | {frame} |",
+    r"             |  ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾  |",
+    r"             |    _________     ________    |",
+    r"    ( (      |   |  latte  |   |espresso|   |",
+    r"     ) )     |   |americano|   | tchibo |   |",
+    r"  .........  |   |capuchino|   | 3 in 1 |   |",
+    r"  |       |] |   | robusta |   | doppio |   |",
+    r"  \       /  |    ‾‾‾‾‾‾‾‾‾     ‾‾‾‾‾‾‾‾    |",
+    r" ‗‗`-----'‗‗‗|______________________________|",
+    r"|___________________________________________|",
+];
+
+
 fn main() {
     call_name_animation(3);
 
@@ -11,26 +52,22 @@ fn main() {
     }
 }
 
-
 fn leave() {
-	clear_entire_screen();
+	clear_screen();
 	move_cursor_up(1000);
     println!("Goodbye, see you later!\n");
     process::exit(0);
 }
 
-
-fn clear_entire_screen() {
+fn clear_screen() {
     print!("\x1b[2J");
     io::stdout().flush().unwrap();
 }
-
 
 fn move_cursor_up(layers: u16) {
 	print!("\x1b[{}A", layers);
 	io::stdout().flush().unwrap();
 }
-
 
 fn choose_coffee() {
 	call_test_choose_coffee_animation(1);
@@ -43,17 +80,12 @@ fn choose_coffee() {
             .read_line(&mut user_choice)
             .expect("Failed to read line");
 
-        match user_choice.as_str().to_lowercase().as_str().trim() {
+        match user_choice.to_lowercase().trim() {
             "leave" => leave(),
             "coffee machine" => call_name_animation(2),
-            "latte" => print_coffee(user_choice.to_lowercase()),
-            "americano" => print_coffee(user_choice.to_lowercase()),
-            "capuchino" => print_coffee(user_choice.to_lowercase()),
-            "robusta" => print_coffee(user_choice.to_lowercase()),
-            "espresso" => print_coffee(user_choice.to_lowercase()),
-            "tchibo" => print_coffee(user_choice.to_lowercase()),
-            "3 in 1" => print_coffee(user_choice.to_lowercase()),
-            "doppio" => print_coffee(user_choice.to_lowercase()),
+            "latte" | "americano" | "capuchino" | "robusta" | "espresso" | "tchibo" | "3 in 1" | "doppio" => {
+                print_coffee(user_choice.to_lowercase());
+            },
             _ => {
                 println!("We don't have this coffee. Please try again!");
                 continue;
@@ -62,97 +94,29 @@ fn choose_coffee() {
     }
 }
 
-
-
-// ASCII Arts
-
-fn ascii_coffee() {
-	clear_entire_screen();
+fn print_ascii_coffee(art: &[&str]) {
+    clear_screen();
 	move_cursor_up(1000);
-    let raw_str_1 = r"                        (";
-    let raw_str_2 = r"                          )     (";
-    let raw_str_3 = r"                   ___...(-------)-....___";
-    let raw_str_4 = r#"               .-""       )    (          ""-."#;
-    let raw_str_5 = r"         .-'``'|-._             )         _.-|";
-    let raw_str_6 = r#"        /  .--.|   `""---...........---""`   |"#;
-    let raw_str_7 = r"       /  /    |                             |";
-    let raw_str_8 = r"       |  |    |                             |";
-    let raw_str_9 = r"        \  \   |                             |";
-    let raw_str_10 = r"         `\ `\ |                             |";
-    let raw_str_11 = r"           `\ `|                             |";
-    let raw_str_12 = r"           _/ /\                             /";
-    let raw_str_13 = r"          (__/  \                           /";
-    let raw_str_14 = r#"       _..---""` \                         /`""---.._"#;
-    let raw_str_15 = r"    .-'           \                       /          \'-.";
-    let raw_str_16 = r"   :               `-.__             __.-'              :";
-    let raw_str_17 = r#"   :                  ) ""---...---"" (                 :"#;
-    let raw_str_18 = r#"    '._               `"--...___...--"`              _.'"#;
-    let raw_str_19 = r#"      \""--..__                              __..--""/"#;
-    let raw_str_20 = r#"       '._     """----.....______.....----"""     _.'"#;
-    let raw_str_21 = r#"          `""--..,,_____            _____,,..--""`"#;
-    let raw_str_22 = r#"                        `"""----"""`"#;
 
-    println!("{}", raw_str_1);
-    println!("{}", raw_str_2);
-    println!("{}", raw_str_3);
-    println!("{}", raw_str_4);
-    println!("{}", raw_str_5);
-    println!("{}", raw_str_6);
-    println!("{}", raw_str_7);
-    println!("{}", raw_str_8);
-    println!("{}", raw_str_9);
-    println!("{}", raw_str_10);
-    println!("{}", raw_str_11);
-    println!("{}", raw_str_12);
-    println!("{}", raw_str_13);
-    println!("{}", raw_str_14);
-    println!("{}", raw_str_15);
-    println!("{}", raw_str_16);
-    println!("{}", raw_str_17);
-    println!("{}", raw_str_18);
-    println!("{}", raw_str_19);
-    println!("{}", raw_str_20);
-    println!("{}", raw_str_21);
-    println!("{}", raw_str_22);
+    for line in art {
+        println!("{}", line);
+    }
 }
 
 #[allow(dead_code)]
 fn print_coffee(coffee: String) {
-    ascii_coffee();
+    print_ascii_coffee(&ASCII_COFFEE);
     println!("\nHere's your {}! Bon appetit!", coffee);
     thread::sleep(time::Duration::from_millis(5000));
-    clear_entire_screen();
+    clear_screen();
     call_test_choose_coffee_animation(1);
     choose_coffee();
 }
 
-
 fn ascii_coffee_machine(frame: &str) {
-    let raw_str_1 = r"  __________________________________________";
-    let raw_str_2 = r" (___________   __________________________  |";
-    let raw_str_3 = format!("   [XXXXX]   | {} |", frame);
-    let raw_str_4 = r"             |  ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾  |";
-    let raw_str_5 = r"             |    _________     ________    |";
-    let raw_str_6 = r"    ( (      |   |  latte  |   |espresso|   |";
-    let raw_str_7 = r"     ) )     |   |americano|   | tchibo |   |";
-    let raw_str_8 = r"  .........  |   |capuchino|   | 3 in 1 |   |";
-    let raw_str_9 = r"  |       |] |   | robusta |   | doppio |   |";
-    let raw_str_10 = r"  \       /  |    ‾‾‾‾‾‾‾‾‾     ‾‾‾‾‾‾‾‾    |";
-    let raw_str_11 = r" ‗‗`-----'‗‗‗|______________________________|";
-    let raw_str_12 = r"|___________________________________________|";
-
-    println!("{}", raw_str_1);
-    println!("{}", raw_str_2);
-    println!("{}", raw_str_3);
-    println!("{}", raw_str_4);
-    println!("{}", raw_str_5);
-    println!("{}", raw_str_6);
-    println!("{}", raw_str_7);
-    println!("{}", raw_str_8);
-    println!("{}", raw_str_9);
-    println!("{}", raw_str_10);
-    println!("{}", raw_str_11);
-    println!("{}", raw_str_12);
+    for line in ASCII_COFFEE_MACHINE.iter() {
+        println!("{}", line.replace("{frame}", frame));
+    }
 }
 
 fn coffee_machine_animation(mut repeat: i8, animation_frames: Vec<&str>) {
@@ -163,7 +127,7 @@ fn coffee_machine_animation(mut repeat: i8, animation_frames: Vec<&str>) {
 			for _i in 0..4 {   // without that animation will be very fast, but if we change fps to 30 it will look bad
 				ascii_coffee_machine(frame);
 				thread::sleep(time::Duration::from_millis(fps));
-				clear_entire_screen();
+				clear_screen();
 				move_cursor_up(1000);
 			};
 		};
